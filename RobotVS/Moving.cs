fc51c249f656c51dda7d94e366e09ba9918d42ed
Fares Steen <fares.steen@tieto.com>
2020-01-16 09:36:47 +0100

    fix text

--- RobotVS/Moving.cs
+++ RobotVS/Moving.cs
@@ -5,12 +5,13 @@ using System.Threading;
 using System.Threading.Tasks;
 namespace RobotVS
 {
- class Moving {
-         int TRIG=17;
-         int ECHO=4;
-       
-         public long distance{get; set;}
-         public bool canMove =false;
+    class Moving
+    {
+        int TRIG = 17;
+        int ECHO = 4;
+
+        public long distance { get; set; }
+        public bool canMove = false;
 
         public bool notStop = true;
         public bool goingBack = false;
@@ -29,83 +30,93 @@ namespace RobotVS
                           {1, 0, 0, 1}
                     };
 
-     GpioController gpio = new GpioController(PinNumberingScheme.Logical, new RaspberryPi3Driver());
+        GpioController gpio = new GpioController(PinNumberingScheme.Logical, new RaspberryPi3Driver());
 
-    public  void Run(){
-        InitMotor();
-        Task task1 = Task.Factory.StartNew(() => StartSonic());
-        Task task2 = Task.Factory.StartNew(() => MoveMotor());
-        // StartSonic();
-        // MoveMotor();
-        Task.WaitAll(task1, task2);
-    }
-       public void StartSonic(){
-       Console.WriteLine("Start Sonic");
-        for (int x=0; x<1000;x++){
-            
-                 gpio.OpenPin(TRIG, PinMode.Output);
-                    gpio.Write(TRIG,0);
+        public void Run()
+        {
+            InitMotor();
+            Task task1 = Task.Factory.StartNew(() => StartSonic());
+            Task task2 = Task.Factory.StartNew(() => MoveMotor());
+            // StartSonic();
+            // MoveMotor();
+            Task.WaitAll(task1, task2);
+        }
+        public void StartSonic()
+        {
+            Console.WriteLine("Start Sonic");
+            for (int x = 0; x < 1000; x++)
+            {
 
-                 gpio.OpenPin(ECHO, PinMode.Input);
+                gpio.OpenPin(TRIG, PinMode.Output);
+                gpio.Write(TRIG, 0);
+
+                gpio.OpenPin(ECHO, PinMode.Input);
 
                 Thread.Sleep(TimeSpan.FromSeconds(0.1));
                 var startScanning = DateTime.Now.AddSeconds(2);
-                
-                gpio.Write(TRIG,1);
-                 Thread.Sleep(TimeSpan.FromSeconds(0.1));
-                 Boolean worked=true;
-                gpio.Write(TRIG,0);
-                
 
-                    while (gpio.Read(ECHO)==0)
+                gpio.Write(TRIG, 1);
+                Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                Boolean worked = true;
+                gpio.Write(TRIG, 0);
+
+
+                while (gpio.Read(ECHO) == 0)
+                {
+
+
+                    if (DateTime.Now > startScanning)
                     {
-                        
-                      
-                        if( DateTime.Now>startScanning){
-                             Console.WriteLine("Failed aftre 2 sec");
-                            gpio.ClosePin(ECHO);
-                             gpio.ClosePin(TRIG);
-                             worked=false;
-                             break;
-                        }
+                        Console.WriteLine("Failed aftre 2 sec");
+                        gpio.ClosePin(ECHO);
+                        gpio.ClosePin(TRIG);
+                        worked = false;
+                        break;
                     }
-                    if(worked){
-                     var start = DateTime.Now;
+                }
+                if (worked)
+                {
+                    var start = DateTime.Now;
 
-                     while (gpio.Read(ECHO)==1)
+                    while (gpio.Read(ECHO) == 1)
                     {
 
                     }
-                     var stop = DateTime.Now;
+                    var stop = DateTime.Now;
                     //Console.WriteLine(stop.Ticks);
 
-                   distance=((stop.Ticks - start.Ticks) *17)/10000;
-                   Console.WriteLine(distance);
+                    distance = ((stop.Ticks - start.Ticks) * 17) / 10000;
+                    Console.WriteLine(distance);
 
-                   gpio.ClosePin(ECHO);
-                   gpio.ClosePin(TRIG);
-                    }
+                    gpio.ClosePin(ECHO);
+                    gpio.ClosePin(TRIG);
+                }
             }
             EndAll();
-            }
-        
+        }
 
-        public void InitMotor(){
+
+        public void InitMotor()
+        {
             Console.WriteLine("Init Motor");
             EndAll();
             notStop = true;
-                 for (int pin = 0; pin < 4; pin++)
-                {
-                    gpio.OpenPin(ControlPin[pin], PinMode.Output);
+            for (int pin = 0; pin < 4; pin++)
+            {
+                gpio.OpenPin(ControlPin[pin], PinMode.Output);
 
-                }
+            }
         }
-       public void MoveMotor(){
-            Console.WriteLine("Move Motor");
+        public void MoveMotor()
+        {
+            bool direction = false;
+
             while (notStop)
+            {
+
+                if (distance > 10 && !goingBack)
                 {
-                if (distance > 10&&!goingBack)
-                {
+
                     for (int halfstep = 7; halfstep > 0; halfstep--)
                     {
                         for (int pin = 0; pin < 4; pin++)
@@ -117,10 +128,13 @@ namespace RobotVS
 
                     }
                 }
-                else {
-                    goingBack = true;
+                else
+                {
+
                     if (distance < 20)
                     {
+                        goingBack = true;
+
                         for (int halfstep = 0; halfstep < 8; halfstep++)
                         {
                             for (int pin = 0; pin < 4; pin++)
@@ -132,37 +146,49 @@ namespace RobotVS
 
                         }
                     }
-                    else {
+                    else
+                    {
                         goingBack = false;
                     }
                 }
-                   
+                if (goingBack != direction)
+                {
+                    if (goingBack)
+                        Console.WriteLine("Move Motor Back");
+                    else
+                        Console.WriteLine("Move Motor forward");
 
+                    direction = goingBack;
                 }
-       }
 
-     
 
-   
+            }
+        }
 
-       public void EndAll(){
-           notStop=false;
+
+
+
+
+        public void EndAll()
+        {
+            notStop = false;
             Console.WriteLine("Ending All");
             for (int pin = 0; pin < 4; pin++)
-                {
+            {
                 try
                 {
                     gpio.ClosePin(ControlPin[pin]);
 
                 }
-                catch (Exception e) {
+                catch (Exception e)
+                {
                     Console.WriteLine(e.Message);
                 }
 
 
-                }
-               
-       }
+            }
+
+        }
 
     }
 }
